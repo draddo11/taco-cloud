1902e08652327841c41218d6e7f20e011b8d53ca
draddo11 <draddo11@gmail.com>
2022-08-25 21:54:10 +0000

    Initial commit

---
+++ src/test/java/appiah/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package appiah.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
