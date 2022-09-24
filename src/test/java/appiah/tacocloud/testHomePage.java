2bb386a3f30aa55c000b3bd6851a19a16624fcf8
draddo11 <draddo11@gmail.com>
2022-09-24 20:31:51 +0000

    reverted server port to default and writing test for homepage

--- src/test/java/appiah/tacocloud/testHomePage.java
+++ src/test/java/appiah/tacocloud/testHomePage.java
@@ -3,14 +3,19 @@ package appiah.tacocloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.Class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 
-@Autowired
-private MockMvc mockMvc;
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
-@Test
-public void testHomePage() throws Exception{
-    mockMvc.perform(get("/"))
+    @Test
+    public void testHomePage() throws Exception{
+        mockMvc.perform(get("/"))
+    }
+
 }
