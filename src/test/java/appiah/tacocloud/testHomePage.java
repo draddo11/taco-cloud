71d04a37e10a9800a4b09254ba6b701c6a0c9bad
draddo11 <draddo11@gmail.com>
2022-09-24 20:37:16 +0000

    testing homecontroller

--- src/test/java/appiah/tacocloud/testHomePage.java
+++ src/test/java/appiah/tacocloud/testHomePage.java
@@ -2,9 +2,11 @@ package appiah.tacocloud;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
